225f71b13c999b59da1f12706e7e5c0b9c781551
PatrykMazurek <33990246+PatrykMazurek@users.noreply.github.com>
2022-10-16 12:44:09 +0200

    Patryk Mazurek
    pliki z zajęć - przekazywanie plików między instancjami klienta i serwera

--- ASI_3_Client/src/App.java
+++ ASI_3_Client/src/App.java
@@ -1,8 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Client c = new Client("localhost", 5501);
-        c.connection();
-        c.sendMessage();
-        c.disconnect();
+//        Client c = new Client("localhost", 5501);
+//        c.connection();
+//        c.sendMessage();
+//        c.disconnect();
+        FileClient fc = new FileClient("localhost", 5501, "pliki");
+        fc.connect();
+        // pobieranie plików z serwera
+        fc.getFileFromServer();
+        // wysyłanie plików do serwera
+        fc.sendFileToServer();
+        fc.disconnect();
+
     }
 }
